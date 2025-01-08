62718d273cec8345cfd9d415e87e8c3d2871e1fb
mStenz <michael.stenz2006@gmail.com>
2025-01-08 13:50:59 +0100

    dev env

---
+++ medusa-config.dev.ts
@@ -0,0 +1,49 @@
+import { loadEnv, defineConfig, Modules } from "@medusajs/framework/utils";
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+export default defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "./src/modules/my-payment",
+            id: "my-payment",
+            options: {
+              // provider options...
+              apiKey: "...",
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: "./src/modules/allergen-module",
+    },
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-local",
+            id: "local",
+            options: {
+            },
+          },
+        ],
+      },
+    },
+  ],
+});
