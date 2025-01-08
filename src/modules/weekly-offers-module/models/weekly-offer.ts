0be97f3c65cd060d17fd5b9d3623c6a45b67e6c2
SIMULATAN <jakob@simulatan.me>
2025-01-08 21:38:30 +0100

    fix(weekly-offer): model products relation

--- src/modules/weekly-offers-module/models/weekly-offer.ts
+++ src/modules/weekly-offers-module/models/weekly-offer.ts
@@ -1,15 +1,10 @@
 import { model } from "@medusajs/framework/utils";
-import { Product } from "@medusajs/medusa";
 
 const WeeklyOffer = model.define("weekly_offer", {
     id: model.id().primaryKey(),
     name: model.text(),
-    products: model.manyToMany(() => Product, {
-        mappedBy: "products",
-        pivotTable: "weekly_offer_product",
-        joinColumn: "offer_id",
-        inverseJoinColumn: "product_id",
-    }),
+    start: model.dateTime(),
+    end: model.dateTime(),
 });
 
 export default WeeklyOffer;
