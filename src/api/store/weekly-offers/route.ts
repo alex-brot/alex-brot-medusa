7a0df1360b036131c67494ce51a75bd58f1d0d0b
lukmayr <l.mayrhuber@students.htl-leonding.ac.at>
2025-01-15 19:50:39 +0100

    fix(weekly-offers): added price and variants to /store/weekly-offers

--- src/api/store/weekly-offers/route.ts
+++ src/api/store/weekly-offers/route.ts
@@ -1,19 +1,39 @@
-import { MedusaRequest, MedusaResponse, Query } from "@medusajs/framework"
-import { ContainerRegistrationKeys } from "@medusajs/framework/utils"
+import { MedusaRequest, MedusaResponse, Query } from "@medusajs/framework";
+import {
+  ContainerRegistrationKeys,
+  QueryContext,
+} from "@medusajs/framework/utils";
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
-  const query = req.scope.resolve<Query>(ContainerRegistrationKeys.QUERY)
+  const query = req.scope.resolve<Query>(ContainerRegistrationKeys.QUERY);
 
   //at the moment only returns id
   const { data } = await query.graph({
     entity: "weekly_offer",
-    fields: ["id", "title", "from", "to", "products.*", ],
-    filters:{
-        from: {
-            $lte: new Date()
+    fields: [
+      "id",
+      "title",
+      "from",
+      "to",
+      "products.*",
+      "products.variants.*",
+      "products.variants.calculated_price.*",
+    ],
+    filters: {
+      from: {
+        $lte: new Date(),
+      },
+    },
+    context: {
+      products: {
+        variants: {
+          calculated_price: QueryContext({
+            currency_code: "eur",
+          }),
         },
-    }
-  })
+      },
+    },
+  });
 
-  return res.json(data)
-}
+  return res.json(data);
+};
