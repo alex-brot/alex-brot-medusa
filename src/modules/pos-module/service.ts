d31eeba188566d08848313d0cadd75df4870b460
SIMULATAN <jakob@simulatan.me>
2025-01-20 10:19:40 +0100

    fix(pos): add EntryTimestamp entity to MedusaService

--- src/modules/pos-module/service.ts
+++ src/modules/pos-module/service.ts
@@ -1,9 +1,11 @@
 import { MedusaService } from "@medusajs/framework/utils";
 import PosAuth from "./models/pos-auth";
+import EntryTimestamp from "./models/entry-timestamp";
 
 
 class PosService extends MedusaService({
     PosAuth,
+    EntryTimestamp,
 }) {}
 
 export default PosService;
