29648b8474677d53abffacee7984ac627731171e
SIMULATAN <jakob@simulatan.me>
2025-01-21 10:22:46 +0100

    fix(pos-auth): createStep must be used inside a createWorkflow definition

--- src/api/admin/pos-auth/route.ts
+++ src/api/admin/pos-auth/route.ts
@@ -1,16 +1,17 @@
 import { MedusaRequest, MedusaResponse, Query } from "@medusajs/framework";
 import { CustomerDTO } from "@medusajs/framework/types";
 import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
-import { POS_MODULE } from "src/modules/pos-module";
-import { createPosAuthWorkflow } from "src/workflows/create-posauth";
+import { createPosAuthWorkflow, CreatePosAuthWorkflowInput } from "src/workflows/create-posauth";
+import { z } from "zod";
 
-export const POST = async (req: MedusaRequest<CustomerDTO>, res: MedusaResponse) => {
+type CreatePosAuthWorkflowInputType = z.infer<typeof CreatePosAuthWorkflowInput>
+export const POST = async (req: MedusaRequest<CreatePosAuthWorkflowInputType>, res: MedusaResponse) => {
   console.log(req.scope);
   console.log(req.body);
 
   const { result } = await createPosAuthWorkflow(req.scope).run({
     input: {
-      customerId: req.body.id,
+      customerId: req.body.customerId,
     }
   })
   return res.status(201).json(result)
--- src/workflows/create-posauth.ts
+++ src/workflows/create-posauth.ts
@@ -1,83 +1,78 @@
-import { PosAuth } from ".medusa/types/query-entry-points";
-import { Modules } from "@medusajs/framework/utils";
-import {
-  createStep,
-  createWorkflow,
-  StepResponse,
-  WorkflowResponse,
-} from "@medusajs/framework/workflows-sdk";
-import { useQueryGraphStep } from "@medusajs/medusa/core-flows";
-import { log } from "console";
-import { POS_MODULE } from "src/modules/pos-module";
-import PosService from "src/modules/pos-module/service";
+import { createStep, createWorkflow, StepResponse, WorkflowResponse, } from "@medusajs/framework/workflows-sdk";
+import PosService from "../modules/pos-module/service";
+import { POS_MODULE } from "../modules/pos-module";
+import { ContainerRegistrationKeys, Modules } from "@medusajs/framework/utils";
+import { PosAuth } from "../../.medusa/types/query-entry-points";
 
-type CreatePosAuthWorkflowInput = {
+export type CreatePosAuthWorkflowInput = {
   customerId: string;
 };
 
 const createPosAuthStep = createStep(
-  "create-posauth-step",
-  async (input: CreatePosAuthWorkflowInput, { container }) => {
-    const posAuthService: PosService = container.resolve(POS_MODULE);
-    let isValidCode = false;
-    let validCode: number | undefined = undefined;
+    "create-posauth-step",
+    async (input: CreatePosAuthWorkflowInput, { container }) => {
+        const posAuthService: PosService = container.resolve(POS_MODULE);
+        let isValidCode = false;
+        let validCode: number | undefined = undefined;
 
-    while (!isValidCode) {
-      const code = Math.floor(1000 + Math.random() * 9000);
-      const { data } = useQueryGraphStep({
-        entity: "pos_auth",
-        fields: ["code"],
-        filters: {
-          code: code,
-        },
-      });
+        const query = container.resolve(ContainerRegistrationKeys.QUERY)
 
-      if (data.length > 0) {
-        isValidCode = true;
-        validCode = code;
-      }
-    }
+        while (!isValidCode) {
+            const code = Math.floor(1000 + Math.random() * 9000);
+            const { data } = await query.graph({
+                entity: "pos_auth",
+                fields: ["code"],
+                filters: {
+                    code: code.toString(),
+                },
+            });
 
-    if (!validCode) {
-      throw new Error("Could not generate a valid code");
-    }
+            if (data.length == 0) {
+                isValidCode = true;
+                validCode = code;
+            }
+        }
 
-    //TODO: fix Type because currently it is not possible to get PosAuth from query-entry-points.d.ts
-    const posAuth: PosAuth | undefined = await posAuthService.createPosAuths({
-      code: validCode,
-      nfcCode: null,
-    });
+        if (!validCode) {
+            throw new Error("Could not generate a valid code");
+        }
 
-    if (!posAuth) {
-      throw new Error("Could not create pos auth");
-    }
-
-    const remoteLinkService = container.resolve("remoteLink");
-
-    const createLink = async (
-      customerId: string,
-      posAuth: PosAuth
-    ) => {
-      try {
-        await remoteLinkService.create({
-          [POS_MODULE]: {
-            pos_auth_id: posAuth.id,
-          },
-          [Modules.PRODUCT]: {
-            customer_id: customerId,
-          },
+        //TODO: fix Type because currently it is not possible to get PosAuth from query-entry-points.d.ts
+        const posAuth: PosAuth | undefined = await posAuthService.createPosAuths({
+            code: validCode,
+            nfcCode: null,
         });
-      } catch (error) {
-        return error;
-      }
 
-      return true;
-    };
+        if (!posAuth) {
+            throw new Error("Could not create pos auth");
+        }
 
-    createLink(input.customerId, posAuth)
+        const remoteLinkService = container.resolve("remoteLink");
 
-    return new StepResponse(true);
-  }
+        const createLink = async (
+            customerId: string,
+            posAuth: PosAuth
+        ) => {
+            try {
+                await remoteLinkService.create({
+                    [POS_MODULE]: {
+                        pos_auth_id: posAuth.id,
+                    },
+                    [Modules.PRODUCT]: {
+                        customer_id: customerId,
+                    },
+                });
+            } catch (error) {
+                return error;
+            }
+
+            return true;
+        };
+
+        await createLink(input.customerId, posAuth)
+
+        return new StepResponse(true);
+    }
 );
 //TODO: create compensation for the case that the step fails
 
