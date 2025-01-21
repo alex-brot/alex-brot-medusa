a67e227f2fab65c2725dc3a91f77e37bdf2fd715
lukmayr <l.mayrhuber@students.htl-leonding.ac.at>
2025-01-21 19:48:35 +0100

    feat(pos-auth): added subscriber to create code on first order

--- src/modules/pos-module/service.ts
+++ src/modules/pos-module/service.ts
@@ -1,11 +1,29 @@
-import { MedusaService } from "@medusajs/framework/utils";
+import { InjectManager, MedusaContext, MedusaService } from "@medusajs/framework/utils";
 import PosAuth from "./models/pos-auth";
 import EntryTimestamp from "./models/entry-timestamp";
+import { Context } from "@medusajs/framework/types";
+import { EntityManager } from "@mikro-orm/knex";
 
 
 class PosService extends MedusaService({
-    PosAuth,
-    EntryTimestamp,
-}) {}
+  PosAuth,
+  EntryTimestamp,
+}) {
+  @InjectManager()
+  async getCountSql(
+    input:{
+        customerId: string;
+    },
+    @MedusaContext() sharedContext?: Context<EntityManager>
+  ): Promise<number> {
+
+    const data = await sharedContext!.manager!.execute(
+      "SELECT COUNT(*) as num FROM public.order where customer_id='" + input.customerId + "'",
+    ) 
+    
+    return parseInt(data[0].num)
+  }
+
+}
 
 export default PosService;
