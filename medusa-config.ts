e8fd27fdc84ef4b96fcaf359baec9acddfee0a6e
mStenz <michael.stenz2006@gmail.com>
2025-01-22 21:08:32 +0100

    [Deployment] add dynamic file extensions for prod env

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,12 +1,9 @@
 import {ConfigModule} from "@medusajs/types";
+import { resolve } from 'path';
 
+const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
+const ending = process.env.NODE_ENV === 'production' ? 'js' : 'ts';
+const configPath = resolve(__dirname, `./medusa-config.${env}.${ending}`);
 
-let config: ConfigModule
-
-if (process.env.NODE_ENV === 'production') {
-  config = require('./medusa-config.prod.ts').default;
-} else {
-  config = require('./medusa-config.dev.ts').default;
-}
-
+const config: ConfigModule = require(configPath).default;
 export default config;
