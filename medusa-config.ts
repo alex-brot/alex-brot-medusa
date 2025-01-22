fe6b20f9332b229759c31f34b9ee4794e167993c
mStenz <michael.stenz2006@gmail.com>
2025-01-22 23:03:20 +0100

    [Deployment] undo env var name

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,8 +1,8 @@
 import {ConfigModule} from "@medusajs/types";
 import { resolve } from 'path';
 
-const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
-const ending = process.env.NODE_ENV === 'production' ? 'js' : 'ts';
+const env = process.env.BUILD_PROD === 'production' ? 'prod' : 'dev';
+const ending = process.env.BUILD_PROD === 'production' ? 'js' : 'ts';
 console.log(process.env.NODE_ENV)
 const configPath = resolve(__dirname, `./medusa-config.${env}.${ending}`);
 
