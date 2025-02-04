f9526ad964be4f4a0a029b066ec2c4f5f2e342db
mStenz <michael.stenz2006@gmail.com>
2025-02-04 13:44:58 +0100

    Add admin cors for deployment

--- medusa-config.prod.ts
+++ medusa-config.prod.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
+      adminCors: process.env.ADMIN_CORS || "http://localhost:9000",
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
