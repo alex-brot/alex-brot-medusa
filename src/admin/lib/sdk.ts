342b98897cf55d93fe4fc349aae3c48f1ff3ec05
mStenz <michael.stenz2006@gmail.com>
2025-02-05 13:20:58 +0100

    use VITE_BACKEND_URL env

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,7 +1,7 @@
 import Medusa from "@medusajs/js-sdk"
 
 export const sdk = new Medusa({
-  baseUrl: "https://dev.medusa.alex-brot.stenz.dev",
+  baseUrl: import.meta.env.VITE_BACKEND_URL || "http://localhost:9000",
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
