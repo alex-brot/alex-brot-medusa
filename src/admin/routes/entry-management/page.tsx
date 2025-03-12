86b3fd33836964d014435042fee53ce1fd623e25
mStenz <michael.stenz2006@gmail.com>
2025-03-12 21:38:12 +0100

    Add 4 Character Check

--- src/admin/routes/entry-management/page.tsx
+++ src/admin/routes/entry-management/page.tsx
@@ -1,10 +1,10 @@
-import { defineRouteConfig } from "@medusajs/admin-sdk";
-import React, { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { sdk } from "../../lib/sdk";
-import { Heading, Input, Table } from "@medusajs/ui";
-import { Customer, PosAuth } from "../../../../.medusa/types/query-entry-points";
-import { UserGroup } from "@medusajs/icons";
+import {defineRouteConfig} from "@medusajs/admin-sdk";
+import React, {useState} from "react";
+import {useQuery} from "@tanstack/react-query";
+import {sdk} from "../../lib/sdk";
+import {Heading, Input, Table} from "@medusajs/ui";
+import {Customer, PosAuth} from "../../../../.medusa/types/query-entry-points";
+import {UserGroup} from "@medusajs/icons";
 
 type EntryManagementResponse = PosAuth[];
 
@@ -46,6 +46,10 @@ const EntryLogPage: React.FC = () => {
 
 
 async function updateCode(id: string, newCode: string) {
+    if (newCode.length !== 4) {
+        return
+    }
+
     await sdk.client.fetch(`/admin/pos-auth/change-code`, {
         method: "POST",
         body: {
@@ -54,10 +58,20 @@ async function updateCode(id: string, newCode: string) {
         },
     })
 }
-
 export const PosAuthTableRow = ({ element }: { element: PosAuth }) => {
     const customer = element.customer as Customer | null
     let [code, setCode] = useState(element.code)
+
+    const [error, setError] = useState<string | null>(null)
+
+    const validateErrorMessages = () => {
+        if (code.length !== 4) {
+            setError("Code must be 4 digits long")
+        } else {
+            setError(null)
+        }
+    }
+
     return (
         <Table.Row>
             {
@@ -65,16 +79,22 @@ export const PosAuthTableRow = ({ element }: { element: PosAuth }) => {
                     <Table.Cell>{customer.first_name} {customer.last_name}</Table.Cell>
                     : <Table.Cell>Customer not found</Table.Cell>
             }
-            <Table.Cell><Input type="number"
-                               min={0}
-                               max={9999}
-                               onChange={async e => {
-                                   const newCode = e.target.value;
-                                   setCode(newCode);
-                               }}
-                               onBlur={e => updateCode(element.id, e.target.value)}
-                               placeholder="entry code"
-                               value={code}/></Table.Cell>
+            <Table.Cell>
+                <Input type="number"
+                       min={0}
+                       max={9999}
+                       onChange={async e => {
+                           const newCode = e.target.value;
+                           setCode(newCode);
+                       }}
+                       onBlur={e => {
+                           validateErrorMessages()
+                           updateCode(element.id, e.target.value)
+                       }}
+                       placeholder="entry code"
+                       value={code}/>
+                {error && <div style={{ color: 'red' }}>{error}</div>}
+            </Table.Cell>
             <Table.Cell>{element.nfcCode ?? "-"}</Table.Cell>
         </Table.Row>
     );
