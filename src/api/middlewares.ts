110daa71b21df0c700515637832f086c73a08fb8
mStenz <michael.stenz2006@gmail.com>
2025-03-12 20:01:10 +0100

    Update lock file

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -1,17 +1,5 @@
-import {
-  authenticate,
-  validateAndTransformBody,
-  validateAndTransformQuery,
-} from "@medusajs/framework";
-import { defineMiddlewares } from "@medusajs/medusa";
-import {
-  GetProductsParams,
-  StoreGetProductParamsDirectFields,
-} from "@medusajs/medusa/api/utils/common-validators/index";
-import { createFindParams } from "@medusajs/medusa/api/utils/validators";
-import { z } from "zod";
-import { PostAdminCreateWeeklyOffer } from "./admin/weekly-offers/validators";
-import { AdminGetProductsParams } from "@medusajs/medusa/api/admin/products/validators";
+import {defineMiddlewares} from "@medusajs/medusa";
+import {createFindParams} from "@medusajs/medusa/api/utils/validators";
 
 export const getProductSchema = createFindParams();
 
